97ca03dd141501e19f614a568e4cee96917c1250
thomastepi <tepexminem2013@gmail.com>
2024-04-04 15:10:14 -0400

    Landing Heading optimized for mobile

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -7,11 +7,13 @@ import img from "../assets/images/coffee-seed.jpg";
 import img2 from "../assets/images/rubber_trees.jpg";
 import img3 from "../assets/images/plantain_bunch.jpg";
 import img4 from "../assets/images/seedling.jpg";
+import useIsMobile from "../hooks/useIsMobile";
 
 const imgURLs = [img, img2, img3, img4];
 
 export const Landing = () => {
   const [currentImgIndex, setCurrentImgIndex] = useState(0);
+  const isMobile = useIsMobile();
 
   useEffect(() => {
     const intervalId = setInterval(() => {
@@ -49,7 +51,7 @@ export const Landing = () => {
           bottom="9"
         >
           <Box fontSize="3xl" py={10}>
-            <Heading fontSize="7xl">Sawyer Camp Farmers</Heading>
+            <Heading fontSize={!isMobile && "7xl"}>Sawyer Camp Farmers</Heading>
             <Heading>Common Initiative Group</Heading>
           </Box>
           <Box display="flex" justifyContent="center" py={6}>
