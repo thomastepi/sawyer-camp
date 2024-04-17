7fb14bdda7cd4b96a21d4f1d87f0103ed65f135d
thomastepi <tepexminem2013@gmail.com>
2024-04-17 03:53:18 -0400

    redesign landing

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,19 +1,25 @@
 import React, { useState, useEffect } from "react";
-import { Heading, Box, Button, HStack, VStack } from "@chakra-ui/react";
+import { Heading, Box, Button, HStack, VStack, Center } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 import img from "../assets/images/rubber_trees.webp";
 import img1 from "../assets/images/cow.jpg";
 import img2 from "../assets/images/pines.jpg";
 import img3 from "../assets/images/fowl.jpg";
 import useIsMobile from "../hooks/useIsMobile";
+import { motion } from "framer-motion";
 
+const MotionBox = motion(Box);
 const imgURLs = [img, img1, img2, img3];
 
 export const Landing = () => {
   const [currentImgIndex, setCurrentImgIndex] = useState(0);
+  const [showItems, setShowItems] = useState(false);
   const isMobile = useIsMobile();
 
   useEffect(() => {
+    setTimeout(() => {
+      setShowItems(true);
+    }, 2000);
     const preloadLinks = imgURLs.map((imageUrl) => {
       const preloadLink = document.createElement("link");
       preloadLink.rel = "preload";
@@ -52,25 +58,55 @@ export const Landing = () => {
         {...backgroundImageStyle}
         position="relative"
       >
-        <Box
-          align="center"
-          justifyContent="center"
-          w="70%"
-          position="absolute"
-          bottom="9"
-        >
-          <Box fontSize="3xl" py={10}>
-            <Heading fontSize={!isMobile && "7xl"}>Sawyer Camp Farmers</Heading>
-            <Heading>Common Initiative Group</Heading>
+        <Center>
+          <Box
+            align="center"
+            justifyContent="center"
+            w="70%"
+            position="absolute"
+            bottom="9"
+          >
+            <MotionBox
+              initial={{ opacity: 0, x: 90 }}
+              animate={{
+                opacity: showItems ? 1 : 0,
+                x: showItems ? 0 : 90,
+              }}
+              transition={{ duration: 0.9 }}
+            >
+              {showItems && (
+                <>
+                  <Box fontSize="3xl" py={10}>
+                    <Heading fontSize={!isMobile && "7xl"}>
+                      Sawyer Camp Farmers
+                    </Heading>
+                    <Heading>Common Initiative Group</Heading>
+                  </Box>
+                </>
+              )}
+            </MotionBox>
+            <MotionBox
+              initial={{ opacity: 0, x: -90 }}
+              animate={{
+                opacity: showItems ? 1 : 0,
+                x: showItems ? 0 : -90,
+              }}
+              transition={{ duration: 0.9 }}
+            >
+              {showItems && (
+                <>
+                  <Box display="flex" justifyContent="center" py={6}>
+                    <HStack spacing={6} color="white">
+                      <Button p={7} colorScheme="green" variant="solid">
+                        <Link to="/membership">Become a Member</Link>
+                      </Button>
+                    </HStack>
+                  </Box>
+                </>
+              )}
+            </MotionBox>
           </Box>
-          <Box display="flex" justifyContent="center" py={6}>
-            <HStack spacing={6} color="white">
-              <Button p={7} colorScheme="green" variant="solid">
-                <Link to="/membership">Become a Member</Link>
-              </Button>
-            </HStack>
-          </Box>
-        </Box>
+        </Center>
       </VStack>
     </>
   );
